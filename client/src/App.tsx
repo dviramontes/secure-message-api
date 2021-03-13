903c630e19ef042cd20db9c5a683e7ef35f197ff
David A. Viramontes <dviramontes@gmail.com>
2021-03-13 15:17:05 -0700

    copy user-uuid to clipboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./App.css";
+
+interface IUsername {
+  username: string;
+}
+
+interface IClipboard {
+  text: string;
+}
 
 const Messages = () => {
   return (
     <div>
       <h4>Messages received</h4>
-      <pre>timestampt -- message</pre>
+      <pre>timestamp -- message</pre>
+    </div>
+  );
+};
+
+const Clipboard = (props: IClipboard) => (
+  <CopyToClipboard text={props.text}>
+    <Button variant="info" size="sm" style={{ marginLeft: "0.5em" }}>
+      <i className="fas fa-clipboard" />
+    </Button>
+  </CopyToClipboard>
+);
+
+const Username = (props: IUsername) => {
+  const uuid = props.username || uuidv4();
+  return (
+    <div>
+      <b>Username:</b> <code>{uuid}</code>
+      <Clipboard text={uuid} />
+    </div>
+  );
+};
+
+const AccountButtons = () => {
+  return (
+    <div>
+      <a href="">Create Account</a> / <a href="">Login</a> /{" "}
+      <a href="">Logout</a>
     </div>
   );
 };
 
 function App() {
+  const [userUUID, setUserUUID] = useState("");
+
   return (
     <div className="app">
       <div className="container">
@@ -19,14 +58,15 @@ function App() {
           <h3>
             Welcome to <u>Secure Messaging App</u> <br />
           </h3>
-          <p>|{" "}<small>v0.0.1</small></p>
+          <p>
+            | <small>v0.0.1</small>
+          </p>
           <p>
             A <em>proof of concept</em> App for sending and receiving messages
             anonymously and securely
           </p>
-          Username: <code>uuid-1234-abcd</code> <br />
-          <a href="">Create Account</a> / <a href="">Login</a> /{" "}
-          <a href="">Logout</a>
+          <Username username={userUUID} />
+          <AccountButtons />
           <div className="section">
             <Messages />
           </div>
